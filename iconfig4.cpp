4717b6d2bfac27d82c6ff7e49f818f4a0ad56d97
Superxy <396415534@qq.com>
2016-04-23 13:07:41 +0800

    template default parameter depend on former template parameter

---
+++ iconfig4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+/*
+ *  non-static data members shall be initialized in the order they were declared in the class definition
+ *  Finally, the compound-statement of the constructor body is executed.
+ * */
+
+class alloc
+{
+
+};
+
+template <typename T, typename Alloc = alloc, size_t Bufsiz = 0>
+class deque
+{
+public:
+	deque() { cout << "deque()" << endl; }
+};
+
+template <typename T, typename Sequence = deque<T> >
+class stack
+{
+public:
+	stack() { cout << "stack()" << endl; }
+private:
+	Sequence c;
+};
+
+
+int main()
+{
+	stack<int> x; //deque()
+		      //stack()
+
+	return 0;
+}
